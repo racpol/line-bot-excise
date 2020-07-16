7f86261f1d7de7181a445ed12a9b0143f2d568fa
racpol <68370059+racpol@users.noreply.github.com>
2020-07-16 14:56:17 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'ju6MpAoy0yBG+JdGIJ/Ha/ECbO8mniDt6aiUp6fcTX5w2v9H6+Bb9XD2W3v8wyodqgEToljLFnywEaN/ZaZJYpxRotSp2MfGCtJnDzFhl5pMHDoQ4kIgeH6LNLnRir6Gzd54zMXLpNMPc2at39glXgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
